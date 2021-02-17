a7221ed6d09d3ae928b5360528a9084d50a3dbe0
[vahermihkel] <[vahermihkel@gmail.com]>
2021-02-17 20:40:04 +0200

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './cart/cart.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+// kandilised sulud on massiivide (listide) jaoks
+// listid koosnevad elementidest, mis on komaga eraldatud
+// elementide arv = komad + 1
+
+// objektid on loogeliste sulgudega, koosnevad võti-väärtus paaridest
+// võti-väärtus paarid on komaga eraldatud
+// kuju: VÕTI KOOLON VÄÄRTUS {võti: väärtus1, võti: väärtus2, võti: väärtus3}
+const routes: Routes = [
+  { path: "", component: HomeComponent},
+  { path: "cart", component: CartComponent},
+  { path: "**", redirectTo: ""}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
