a7a5f5b42279e4949c869e1121654947c6772c40
mihkelvaher <mihkel.vaher@industry62.com>
2021-03-24 20:24:55 +0200

    category filter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import { ThousandSeparatorPipe } from './pipes/thousand-separator.pipe';
 import { ShortenTitlePipe } from './pipes/shorten-title.pipe';
 import { ViewComponent } from './item/view/view.component';
 import { UniqueCategoryPipe } from './pipes/unique-category.pipe';
+import { SpinnerComponent } from './shared/spinner/spinner.component';
 
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http);
@@ -39,7 +40,8 @@ export function HttpLoaderFactory(http: HttpClient) {
     ThousandSeparatorPipe,
     ShortenTitlePipe,
     ViewComponent,
-    UniqueCategoryPipe
+    UniqueCategoryPipe,
+    SpinnerComponent
   ],
   imports: [
     BrowserModule,
@@ -55,7 +57,7 @@ export function HttpLoaderFactory(http: HttpClient) {
     }
     })
   ],
-  providers: [],
+  providers: [UniqueCategoryPipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '../cart/cart.service';
 import { Item } from '../models/item.model';
+import { UniqueCategoryPipe } from '../pipes/unique-category.pipe';
 import { ItemService } from '../services/item.service';
 
 @Component({
@@ -9,47 +10,72 @@ import { ItemService } from '../services/item.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  items: Item[] = [];
+  itemsOriginal: Item[] = [];
+  itemsShown: Item[] = [];
   titleSortNumber = 0;
   priceSortNumber = 0;
+  itemCategories!: {category: string, isSelected: boolean}[];
+  isLoading = false;
   // kuupaev = new Date();
 
   constructor(private cartService: CartService,
-    private itemService: ItemService) { }
+    private itemService: ItemService,
+    private uniqueCategoryPipe: UniqueCategoryPipe) { }
 
   ngOnInit(): void {
-    // this.items = this.itemService.itemsInService.slice();
-    // this.itemService.saveItemsToDatabase();
+    this.isLoading = true;
     this.itemService.getItemsFromDatabase().subscribe(itemsFromFirebase => {
-      this.items = [];
+      this.itemsOriginal = [];
       this.itemService.itemsInService = [];
       for (const key in itemsFromFirebase) {
           const element = itemsFromFirebase[key];
-          this.items.push(element);
+          this.itemsOriginal.push(element);
           this.itemService.itemsInService.push(element);
       }
-      // this.items = this.itemService.itemsInService;
-      // this.itemService.itemsInService = items;
+      this.itemsShown = this.itemsOriginal.slice();
+  //    [{title: "PEALKIRI", price: 49, category: "shoes"},{title: "PEALKIRI", price: 50, ...},{title: "MUU", price: 50, ...}]
+  // ["shoes","beer", "car"]
+  // [{category: "shoes", isSelected: true},{category: "beer", isSelected: true}, "car"]
+      this.itemCategories = this.uniqueCategoryPipe.transform(this.itemsOriginal).map(itemCategory => {
+        return {category: itemCategory, isSelected: true}
+      });
+      this.isLoading = false;
     })
   }
+
+  onSelectCategory(index: number) {
+    this.itemCategories[index].isSelected = !this.itemCategories[index].isSelected;
+    this.itemsShown = this.itemsOriginal.filter(item=>{
+      let category = this.itemCategories.find(itemCategory => {
+          return item.category == itemCategory.category;
+      })
+      if (category?.isSelected == true) {
+        return item;
+      } else {
+        return null;
+      }
+    })
+    // this.itemsShown = this.itemsOriginal
+  }
+  
 
   onSortTitle() {
     // sort(saab esimese, teise eseme) =>
     // funktsiooni ning tagastab võrreldud esemed
     // ,1,10,1123,1231,1231,1,123,123
     if (this.titleSortNumber == 0) {
-      this.items = this.items.sort((a, b) => 
+      this.itemsShown = this.itemsShown.sort((a, b) => 
       a.title.localeCompare(b.title)
       //ASCII
       );
       this.titleSortNumber = 1;
     } else if (this.titleSortNumber == 1) {
-      this.items = this.items.sort((a, b) => 
+      this.itemsShown = this.itemsShown.sort((a, b) => 
       b.title.localeCompare(a.title)
       );
       this.titleSortNumber = 2;
     } else {
-      this.items = this.itemService.itemsInService.slice();
+      this.itemsShown = this.itemService.itemsInService.slice();
       this.titleSortNumber = 0;
     }
     
@@ -57,13 +83,13 @@ export class HomeComponent implements OnInit {
 
   onSortPrice() {
     if (this.priceSortNumber == 0) {
-      this.items.sort((a, b) => a.price - b.price);
+      this.itemsShown.sort((a, b) => a.price - b.price);
       this.priceSortNumber = 1;
     } else if (this.priceSortNumber == 1) {
-      this.items.sort((a, b) => b.price - a.price);
+      this.itemsShown.sort((a, b) => b.price - a.price);
       this.priceSortNumber = 2;
     } else {
-      this.items = this.itemService.itemsInService.slice();
+      this.itemsShown = this.itemService.itemsInService.slice();
       this.priceSortNumber = 0;
     }
   }
